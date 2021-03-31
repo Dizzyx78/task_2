4727bb9c68a0d666c21272d5db48ba916df345d5
dizzyx78 <dizzyx78@gmail.com>
2021-03-31 12:39:08 +0500

    first task

--- spiral.py
+++ spiral.py
@@ -1,6 +1,6 @@
 turn = 0
-s = 1
-flag = 0
+number = 1
+line = 0
 
 while True:
     try:
@@ -14,29 +14,29 @@ while True:
 print("Вы ввели:",n)
 turn = n+n-1
 print("Количество поворотов в спирале:",turn)
-A = [[0] * n for i in range(n)]
+A = [[0] * n for a in range(n)]
 
 while turn>=0:
-    for k in range (flag,n-flag):
-        A[flag][k] = s
-        s+=1
+    for c in range (line,n-line):
+        A[line][c] = number
+        number+=1
     turn-=1
-    for k in range(1+flag,n-flag):
-        A[k][n-1-flag] = s
-        s+=1
+    for c in range(1+line,n-line):
+        A[c][n-1-line] = number
+        number+=1
     turn -= 1
-    for k in range (n-2-flag,-1+flag,-1):
-        A[n-1-flag][k] = s
-        s+=1
+    for c in range (n-2-line,-1+line,-1):
+        A[n-1-line][c] = number
+        number+=1
     turn -= 1
-    for k in range (n-2-flag,0+flag,-1):
-        A[k][flag] = s
-        s+=1
+    for c in range (n-2-line,0+line,-1):
+        A[c][line] = number
+        number+=1
     turn -= 1
-    flag+=1
+    line+=1
 
 print("Итоговый результат:")
-for i in range(n):
-    for j in range(n):
-        print(A[i][j], end="\t")
+for a in range(n):
+    for b in range(n):
+        print(A[a][b], end="\t")
     print("\n")
